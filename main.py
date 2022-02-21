44e65d2b5ebe7b6b7c8ad1f197bd2289521fb3d5
OlinRB <olinrb@live.com>
2022-02-20 23:36:50 -0500

    handshake -> test on silk

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+import socketserver
+
 """
 UVMPM cmd line chat interface
 
+servers listening at port: 12000
+
+Handshake->Authentication->Request/Response
+Server IP: 132.198.11.12–
+
 """
+
+import socket
+import sys
+
+HOST, PORT = "132.198.11.12", 12000
+data = "m".join(sys.argv[1:])
+
+print(data)
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes("m" + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
+
+
+
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
