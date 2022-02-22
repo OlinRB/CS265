6b58d996b0eea90eb3300d5c23d50f822db6445a
OlinRB <olinrb@live.com>
2022-02-22 17:31:25 -0500

    .

--- test.py
+++ test.py
@@ -7,12 +7,17 @@ def connect():
     HOST, PORT = "132.198.11.12", 12000
     connection.connect((HOST, PORT))
     return connection
-while True:
+def main():
     connection = connect()
-    readers,_,_= select.select([sys.stdin,connection],[],[])
-    for reader in readers:
-        if reader is connection:
-            print(connection.recv(1000).decode("utf8"))
-        else:
-            msg = sys.stdin.readline()
-            connection.send(msg.encode("utf8"))
+    print("Success")
+    while True:
+
+        readers,_,_= select.select([sys.stdin,connection],[],[])
+        for reader in readers:
+            if reader is connection:
+                print(connection.recv(1000).decode("utf8"))
+            else:
+                msg = sys.stdin.readline()
+                connection.send(msg.encode("utf8"))
+
+main()
