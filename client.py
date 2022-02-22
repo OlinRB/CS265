809da20aced7f2129e342be335fa6461bde2ecb2
OlinRB <olinrb@live.com>
2022-02-22 17:06:11 -0500

    fixes

--- client.py
+++ client.py
@@ -8,25 +8,17 @@ class Client:
         self.HOST = HOST
         self.PORT = PORT
         self.Init_Connection()
-        self.received = []
+
 
     def Init_Connection(self):
-        pass
         self.s = socket.socket()
         self.s.connect((self.HOST, self.PORT))
 
     def Send_Data(self, message):
         self.s.sendall(bytes(message + "\n", "utf-8"))
-        received = str(self.s.recv(1024), "utf-8").strip()
-        if self.received == "HELLO":
-            self.initialized = True
-
-        if self.received == "AUTHYES":
-            self.authenticated = True
-            received = str(self.s.recv(1024), "utf-8").strip()
-
-    def Receive(self):
-        self.received = str(self.s.recv(1024), "utf-8").strip()
+    #
+    # def Receive(self):
+    #     self.received = str(self.s.recv(1024), "utf-8").strip()
 
     def __del__(self):
         self.s.close()
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Handshake->Authentication->Request/Response
 Server IP: 132.198.11.12–
 
 """
+
 from client import Client
 import select
 import sys
@@ -20,15 +21,25 @@ def connect():
     # HOST = input("Please enter the server IPv4 address: ")
     # PORT = input("Please enter the port number: ")
     HOST, PORT = "132.198.11.12", 12000
+    # client = socket.socket()
+    # client.connect((HOST, PORT))
     client = Client(HOST, PORT)
-    client.Send_Data("HELLO")
+    message = "HELLO"
+    client.Send_Data(message)
+    print(str(client.s.recv(1024), "utf-8").strip())
+    # client.sendall(bytes(message + "\n", "utf-8"))
+    # print(str(client.recv(1024), "utf-8").strip())
     return client
 
 def authenticate(client):
     un = input("Please enter your username: ")
     pwd = input("Please enter your password: ")
     un_pwd = "AUTH:{}:{}".format(un,pwd)
-    client.Send_Data(un_pwd)
+    client.s.sendall(bytes(un_pwd + "\n", "utf-8"))
+    #print(str(client.s.recv(1024), "utf-8").strip())
+    if str(client.s.recv(1024), "utf-8").strip() == "AUTHYES":
+        client.authenticated = True
+
 
 def menu_options():
     print("Choose an option")
@@ -47,31 +58,24 @@ def test(client):
 
 def main():
 
-    client = connect()
-    # client = socket.socket()
-    # HOST, PORT = "132.198.11.12", 12000
-    # client.connect((HOST, PORT))
 
-    data = "AUTH:test1:p001"
-    # client.Send_Data(data)
-    # while not client.authenticated:
-    #     authenticate(client)
+    client = connect()
+
+    while not client.authenticated:
+        authenticate(client)
     run = True
     while run:
         #menu_options()
         readers, _, _ = select.select([client.s], [], [])
-        print(type(readers))
         for reader in readers:
             if reader is client:
-                client.Receive()
-                print(client.received)
+                print(client.s.recv(1000).decode("utf8"))
             else:
                 i = sys.stdin.readline()
                 if i == "exit":
                     run = False
                 else:
                     client.Send_Data(i)
-            exit()
 
 
 
