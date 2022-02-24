79875d8de83d9e41eae9bf521adda3795807de9d
OlinRB <olinrb@live.com>
2022-02-24 18:04:39 -0500

    transfer to silk

--- u_input.py
+++ u_input.py
@@ -7,7 +7,14 @@ class Input():
     def fileno(self):
         return sys.stdin.fileno()
 
+    def Menu(self):
+        print("\nPlease choose an option:")
+        print("1) List online users")
+        print("2) Send a message")
+        print("3) Sign off")
+
 
     def Read_Data(self):
         print("\n")
+        self.Menu()
         self.client.Send_Data()
