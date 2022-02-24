0c8bca512fb91e5e730feac0e7c1f858016ce0f0
OlinRB <olinrb@live.com>
2022-02-23 23:09:31 -0500

    .

--- chat.py
+++ chat.py
@@ -10,14 +10,14 @@ from input import Input
 def main():
     HOST, PORT = "132.198.11.12", 12000
     client = Client(HOST, PORT)
-    #client.Init_Connection()
-    while not client.authenticated:
-        client.Authenticate()
-    input = Input(client)
-    connection = Loop()
-    connection.Add_Reader(client)
-    connection.Add_Reader(input)
-    connection.Run()
+    # #client.Init_Connection()
+    # while not client.authenticated:
+    #     client.Authenticate()
+    # input = Input(client)
+    # connection = Loop()
+    # connection.Add_Reader(client)
+    # connection.Add_Reader(input)
+    # connection.Run()
 
 
 main()
--- chatStarter.py
+++ chatStarter.py
@@ -3,17 +3,26 @@ import sys
 import select
 
 def main():
-    client = socket.socket()
-    HOST, PORT = "132.198.11.12", 12000
-    client.connect((HOST, PORT))
-    while True:
-        readers, _, _ = select.select([sys.stdin, client], [], [])
-        for reader in readers:
-            if reader is client:
-                print(str(client.recv(1024), "utf-8"))
-                print(">", end=" ")
-            else:
-                data = sys.stdin.readline()
-                client.send(data.encode("utf-8"))
-
+    oop = False
+    if oop:
+        client = socket.socket()
+        HOST, PORT = "132.198.11.12", 12000
+        client.connect((HOST, PORT))
+        while True:
+            readers, _, _ = select.select([sys.stdin, client], [], [])
+            for reader in readers:
+                if reader is client:
+                    print(str(client.recv(1024), "utf-8"))
+                    print(">", end=" ")
+                else:
+                    data = sys.stdin.readline()
+                    client.send(data.encode("utf-8"))
+    else:
+        HOST, PORT = "132.198.11.12", 12000
+        s = socket.socket()
+        s.connect((HOST, PORT))
+        data = "HELLO\n".encode("utf-8")
+        s.send(data)
+        response = str(s.recv(1024), "utf-8")
+        print(response)
 main()
